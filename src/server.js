9e0450ec13c03e0e97925d9a4a609e57d1f34aa9
eung-eung <boyvip5231@gmail.com>
2025-09-18 14:20:06 +0700

    init base project

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 const port = 3000
 const localhost = 'localhost'
 
 app.get('/', (req, res) => {
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
   return res.send('Hello World!')
 })
 
