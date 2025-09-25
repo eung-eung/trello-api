2dd751678cfc9a9811861e07bd4281956a5f5798
eung-eung <boyvip5231@gmail.com>
2025-09-25 15:49:10 +0700

    error handling middleware

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,14 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
 
+
 const createNew = (req, res, next) => {
 
   try {
-    console.log(req.body)
     res.status(StatusCodes.CREATED).json({ message: 'POST from controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })}
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joy from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joy.object({
@@ -13,11 +14,7 @@ const createNew = async (req, res, next) => {
     await correctCondition.validateAsync(req.body, { abortEarly: false })
     next()
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
