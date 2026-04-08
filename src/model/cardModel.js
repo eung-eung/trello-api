86a5de3bc22a5dfa669d0b50924a9eb457e8e8d7
eung-eung <boyvip5231@gmail.com>
2026-04-08 14:11:53 +0700

    feat(card): delete cards by columnId

--- src/model/cardModel.js
+++ src/model/cardModel.js
@@ -80,10 +80,24 @@ const update = async (cardId, updatedData) => {
   } catch (error) { throw new Error(error) }
 }
 
+const deleteManyByColumnId = async (columnId) => {
+  try {
+    await GET_DB().collection(CARD_COLLECTION_NAME).updateMany(
+      { columnId: new ObjectId(String(columnId)) },
+      {
+        $set: { _destroy: true },
+        $currentDate: { updatedAt: true, deletedAt: true }
+      }
+    )
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 export const cardModel = {
   CARD_COLLECTION_NAME,
   CARD_COLLECTION_SCHEMA,
   createNew,
   findOneById,
-  update
+  update,
+  deleteManyByColumnId
 }
