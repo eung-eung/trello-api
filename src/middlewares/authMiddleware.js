50272e4421e550a905afbcb195a0489ef479f299
eung-eung <boyvip5231@gmail.com>
2026-04-22 16:11:32 +0700

    feat: add authorization middleware for protected routes

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+import { JwtProvider } from '~/providers/JwtProvider'
+import { env } from '~/config/environment'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
+
+const isAuthorized = async (req, res, next) => {
+  // lấy access token nằm trong request cookie phía client - withCredentials: true
+  const clientAccessToken = req.cookies?.accessToken
+  if (!clientAccessToken) {
+    next(new ApiError(StatusCodes.UNAUTHORIZED, 'Unauthorized! Token not found'))
+    return
+  }
+  try {
+    //verify token
+    const accessTokenDecoded = await JwtProvider.verifyToken(
+      clientAccessToken,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE
+    )
+    console.log(accessTokenDecoded)
+    //hợp lệ => lưu thông tin giải mã vào req để các tầng sau có thể sử dụng
+    req.jwtDecoded = accessTokenDecoded
+
+    //cho request đi tiếp
+    next()
+  } catch (error) {
+    console.log('auth middleware: ', error)
+    //accessToken expire => trả mã lỗi để FE biết gọi refresh Token
+    if (error?.message?.includes('jwt expired')) {
+      next(new ApiError(StatusCodes.GONE, 'Need to refresh token'))
+    }
+
+    //accessToken không hợp lệ ngoài việc bị expire, 401 cho client gọi sign out
+    next(new ApiError(StatusCodes.UNAUTHORIZED, 'Unauthorized'))
+  }
+}
+
+export const authMiddleware = {
+  isAuthorized
+}
