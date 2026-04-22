86fb05bc5d9a948dc91d2577bc88142a01d624e0
eung-eung <boyvip5231@gmail.com>
2026-04-22 16:18:13 +0700

    refactor: add shared auth constants and utilities

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
+import { env } from '~/config/environment'
+
 export const API = 'AAAA'
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173'
 ]
-
+export const WEBSITE_DOMAINS = (env.BUILD_MODE === 'production') ? env.WEBSITE_URL_PRODUCTION : env.WEBSITE_URL_DEVELOPMENT
 export const BOARD_TYPES = ['private', 'public']
--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,5 +1,12 @@
+import { pick } from 'lodash'
 import slugify from 'slugify'
 
 export const formatFromStringtoSlug = (str) => {
   return slugify(str, { lower: true, strict: false, locale: 'vi' })
+}
+
+export const pickUser = (user) => {
+  if (!user) return { }
+  return pick(user, ['_id', 'email', 'username', 'displayName', 'avatar',
+    'role', 'isActive', 'createdAt', 'updatedAt'])
 }
